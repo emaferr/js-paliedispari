1c4178abfa5cd42626706119417f68ced527bda7
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-29 17:36:08 +0200

    Creata funzione numero random

--- main.js
+++ main.js
@@ -42,6 +42,23 @@ document.write('la parola è palindroma');
 document.write('la parola non è palindroma');
 }
 
-  
 
-  
+
+// Creo la funzione per generare un numero random intero
+function randomNumber(minimum_number, max_number) {
+  minimum_number = Math.ceil(minimum_number);
+  max_number = Math.floor(max_number);
+  return Math.floor(Math.random() * (max_number - minimum_number + 1)) + minimum_number; //Il max è incluso e il min è incluso
+  }
+
+
+// L’utente sceglie pari o dispari
+var userChoice =  prompt("Scegli! pari o dispari?");
+
+// inserisce un numero da 1 a 5.
+var userNumber =  Number(prompt("Scegli un numero da 1 a 5"));
+
+// Invoco la funzione per generare il numero casuale
+var computerNumber = randomNumber(1, 5);
+console.log(computerNumber);
+    
