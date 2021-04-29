30de4fc6e9580610ccce2711ae4765eb17144b4a
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-29 20:34:08 +0200

    Impostata condizione che determina il vincitore

--- main.js
+++ main.js
@@ -51,14 +51,45 @@ function randomNumber(minimum_number, max_number) {
   return Math.floor(Math.random() * (max_number - minimum_number + 1)) + minimum_number; //Il max è incluso e il min è incluso
   }
 
+// Creo la funzione che stabilisce se la somma di due numeri e pari o dispari
+function evenOdd(number_one, number_two) {
+    var rest = ((number_one + number_two) % 2);
+
+    if (rest === 0){
+        return "pari";
+    }else {
+        return "dispari"
+    }
+
+    }
 
 // L’utente sceglie pari o dispari
-var userChoice =  prompt("Scegli! pari o dispari?");
+var userChoice =  prompt("Scegli! pari o dispari?").toLocaleLowerCase();
 
 // inserisce un numero da 1 a 5.
 var userNumber =  Number(prompt("Scegli un numero da 1 a 5"));
 
 // Invoco la funzione per generare il numero casuale
 var computerNumber = randomNumber(1, 5);
-console.log(computerNumber);
+
+// Invoco la funzione per generare un esito
+var esito = evenOdd(userNumber, computerNumber);
+
+console.log(userChoice);
+console.log(esito);
+
+// Stampo a schermo se l'utente ha vinto o perso impostando una condizione
+if (userChoice == esito){
+    document.write(" Hai vinto")
+}else {
+    document.write(" Hai perso")
+}
+
+
+
+
+
+
+
+
     
